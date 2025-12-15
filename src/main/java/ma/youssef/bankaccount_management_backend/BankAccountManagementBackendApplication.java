391e8013be688f08122a1d98f43cb0a2996c23d9
Youssef Ait Said <saidyoussef642@gmail.com>
2025-12-15 11:59:25 +0100

    Fisrt Commit

---
+++ src/main/java/ma/youssef/bankaccount_management_backend/BankAccountManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package ma.youssef.bankaccount_management_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAccountManagementBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankAccountManagementBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/youssef/bankaccount_management_backend/BankAccountManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.youssef.bankaccount_management_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAccountManagementBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
